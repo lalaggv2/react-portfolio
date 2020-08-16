46563f8c0a36ad8e8b7e40995bd88e7003aff028
Lala <luangu2020@gmail.com>
2020-08-16 18:08:21 -0400

    Updates

---
+++ precache-manifest.1ca8dd50adeb1a2c1cad88ab4f064791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abb7815cf691d4ca0654eafc4b75eda",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "154ac2575cd79fef2416",
+    "url": "/react-portfolio/static/css/main.e4c166a0.chunk.css"
+  },
+  {
+    "revision": "44f24f98a14641988e55",
+    "url": "/react-portfolio/static/js/2.3c63b85d.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/react-portfolio/static/js/2.3c63b85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154ac2575cd79fef2416",
+    "url": "/react-portfolio/static/js/main.8c6e094f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
