b2a09d658dcc2519f0c41776dd49b497013c3cb1
Lala <luangu2020@gmail.com>
2020-08-18 22:55:33 -0400

    trying to get the images and resume to work locall

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { ArrowRight16 } from '@carbon/icons-react';
 
+
 import { HeaderContainer, Header, Image, ViewResumeLink } from './styles';
 
 const UserHeader = ({ user }) => {
@@ -21,12 +22,12 @@ const UserHeader = ({ user }) => {
           <p>Coding in {user.basics.region}</p>
           <p>{user.basics.yearsOfExperience} years of experience as a developer</p>
           <p>{user.basics.headline}</p>
-          <p>
+          {/* <p>
             Blog:{' '}
             <a href={user.basics.blog} target="_blank" rel="noreferrer noopener">
               {user.basics.blog}
             </a>
-          </p>
+          </p> */}
         </div>
       </Header>
       <div>
@@ -35,6 +36,7 @@ const UserHeader = ({ user }) => {
           target="_blank"
           rel="noopener noreferrer"
         >
+
           <span>View Résumé</span>
           <ArrowRight16 />
         </ViewResumeLink>
